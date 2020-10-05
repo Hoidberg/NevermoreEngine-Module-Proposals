a95381b3cadedc10384512b1badc36bd6f29fcef
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-05 08:57:19 -0500

    Update init.spec.lua

--- src/Clicky/init.spec.lua
+++ src/Clicky/init.spec.lua
@@ -1,36 +1,7 @@
 return function()
 	local Clicky = require(script.Parent)
-	local placeholder = "I don't know what to put here lol"
 	
 	it("creates a ClickDetector and fires the given function when it is clicked", function()
 		expect(Clicky).to.be.ok()
-	end)
-	
-	describe("object", function()
-		it("contains the ClickDetector", function()
-			local ClickDetector = Clicky.new(Instance.new("Part", workspace), function(times)
-				if times == 1 then
-					print(placeholder)
-				else
-					print(placeholder .. " x" .. times)
-				end
-			end)
-
-			expect(ClickDetector._obj).to.be.ok()
-		end)
-	end)
-	
-	describe("callback", function()
-		it("should be fired when the given object is clicked", function()
-			local ClickDetector = Clicky.new(Instance.new("Part", workspace), function(times)
-				if times == 1 then
-					print(placeholder)
-				else
-					print(placeholder .. " x" .. times)
-				end
-			end)
-
-			expect(ClickDetector).to.be.ok()
-		end)
 	end)
 end
