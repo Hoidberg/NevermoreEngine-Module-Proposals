d152a9dae83d9ea79ecc8c0af44805dd05780f7a
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-05 13:52:12 -0500

    Update init.spec.lua

--- src/Clicky/init.spec.lua
+++ src/Clicky/init.spec.lua
@@ -1,5 +1,5 @@
 return function()
-  local Clicky = require(script.Parent)
+  local Clicky = require(script.Parent.init)
 
   it("should fire the given function when the object is clicked", function()
     expect(Clicky).to.be.ok()
