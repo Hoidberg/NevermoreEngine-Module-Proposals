a154be9ea9f98b835f928b5f17eb140041573c5f
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-10-06 08:23:14 -0500

    Delete init.spec.lua

--- src/Clicky/init.spec.lua
+++
@@ -1,7 +0,0 @@
-return function()
-  local Clicky = require(script.Parent)
-
-  it("should fire the given function when the object is clicked", function()
-    expect(Clicky).to.be.ok()
-  end)
-end
